5f147d19467dd96a57997de990f92cb47384f26c
MatLeg <mat.legierskii@gmail.com>
2023-11-15 20:21:33 +0100

    todo added

--- app/src/main/java/com/example/sportevents/util/extension/ZonedDateTimeExtension.kt
+++ app/src/main/java/com/example/sportevents/util/extension/ZonedDateTimeExtension.kt
@@ -3,12 +3,14 @@ package com.example.sportevents.util.extension
 import com.example.sportevents.domain.enum.DayType
 import java.time.ZonedDateTime
 
+//TODO display leading zeros for hour and minute
 fun ZonedDateTime.getFormattedDate(): String {
     val dayType = DayType.fromZonedDateTime(this)
+    fun getFormattedTime() = "${this.hour}:${this.minute}"
     return when (dayType) {
-        DayType.TOMORROW -> "Tomorrow, ${this.hour}:${this.minute}"
-        DayType.YESTERDAY -> "Yesterday, ${this.hour}:${this.minute}"
-        DayType.TODAY -> "Today, ${this.hour}:${this.minute}"
+        DayType.TOMORROW -> "Tomorrow, ${getFormattedTime()}"
+        DayType.YESTERDAY -> "Yesterday, ${getFormattedTime()}"
+        DayType.TODAY -> "Today, ${getFormattedTime()}"
         DayType.OTHER -> "${this.dayOfMonth}.${this.month}.${this.year}"
     }
 }
