e260a3e3f384435f0ea11d010b166bbb89e92141
MatLeg <mat.legierskii@gmail.com>
2023-11-16 20:40:04 +0100

    implemented event image using the Coil library

--- app/src/main/java/com/example/sportevents/util/component/EventItem.kt
+++ app/src/main/java/com/example/sportevents/util/component/EventItem.kt
@@ -1,10 +1,13 @@
 package com.example.sportevents.util.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,15 +17,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.sportevents.R
 import com.example.sportevents.domain.model.SportEvent
 import com.example.sportevents.util.extension.getFormattedDate
 import java.time.ZonedDateTime
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 @Preview
 fun EventItem(
@@ -46,7 +54,18 @@ fun EventItem(
                 .size(72.dp)
                 .background(Color.LightGray)
         ) {
-            Text(text = "VIDEŁO")
+            Image(
+                painter = rememberImagePainter(
+                    data = event.imageUrl,
+                    builder = {
+                        crossfade(true)
+                        error(R.drawable.ic_launcher_background) //todo set error image
+                    }
+                ),
+                contentDescription = event.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxHeight().aspectRatio(1f)
+            )
         }
         Column() {
             Text(
